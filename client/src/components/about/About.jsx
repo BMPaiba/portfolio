2192d00737cc57b6639e577bf363a7563075a7e9
Brian Paiba <brianmanuelpaiba@outlook.es>
2024-03-02 09:23:00 -0300

    third commit

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./About.module.css";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+
 export default function About() {
   const description =
     "¡Hola! Soy Brian, un apasionado desarrollador web con una sólida formación en tecnologías como JavaScript, React, Redux, Node.js, Express, Sequelize y PostgreSQL. Me destaco por mi enfoque centrado en la calidad y la eficiencia en cada proyecto en el que me involucro. Estoy siempre buscando nuevas oportunidades para aplicar y expandir mis habilidades en el desarrollo web. ";
--- client/src/components/project/Project.jsx
+++ client/src/components/project/Project.jsx
@@ -8,38 +8,38 @@ import { FaArrowsRotate } from "react-icons/fa6";
 import { MdOutlinePlayCircle } from "react-icons/md";
 
 export default function Project() {
-  
   return (
     <>
+      <h2>Proyectos</h2>
       <div className={styles.project}>
         <div className={styles.project_itemI}>
           <img src={rickMortyImg} alt="" />
           <h4>Rick & Morty</h4>
           <div className={styles.project_itemI_buttons}>
-          <button >
-            <FaGithub />
-          </button>
-          <button >
-            <FaArrowsRotate  />
-          </button>
-          <button  >
-            <FaExternalLinkAlt />
-          </button>
+            <button>
+              <FaGithub />
+            </button>
+            <button>
+              <FaArrowsRotate />
+            </button>
+            <button>
+              <FaExternalLinkAlt />
+            </button>
           </div>
         </div>
         <div className={styles.project_itemII}>
           <img src={countries} alt="" />
           <h4>Proyecto de Bootcamp</h4>
           <div className={styles.project_itemII_buttons}>
-          <button >
-            <FaGithub />
-          </button>
-          <button >
-            <FaArrowsRotate  />
-          </button>
-          <button  >
-            <FaExternalLinkAlt />
-          </button>
+            <button>
+              <FaGithub />
+            </button>
+            <button>
+              <FaArrowsRotate />
+            </button>
+            <button>
+              <FaExternalLinkAlt />
+            </button>
           </div>
         </div>
         <div className={styles.project_itemIII}>
@@ -48,13 +48,13 @@ export default function Project() {
         </div>
         <div className={styles.project_itemIV}></div>
         <div className={styles.project_itemII_buttons}>
-          <button >
+          <button>
             <MdOutlinePlayCircle />
           </button>
-          <button  >
+          <button>
             <FaArrowsRotate />
           </button>
-          </div>
+        </div>
       </div>
     </>
   );
