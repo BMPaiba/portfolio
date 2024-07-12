3044ab53ac9d1b6090e378b73d729e6cfb8342cf
brianmp <brianmanuelpaiba@outlook.es>
2024-07-12 00:18:05 -0300

    primera parte, realizacion de Home casi terminada

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from "./Header.astro"
+export { default as Badge } from "./Badge.astro"
+export { default as BadgeRedes } from "./BadgeRedes.astro"
+export { default as Home } from "./Home.astro"
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"Green": 'rgb(177 227 146)',
+			}
+		}
 	},
 	plugins: [],
 }
